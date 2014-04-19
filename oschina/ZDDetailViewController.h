73f1b822e022113a88ffa8ed4c8736f262fdd6f4
royalwang <master@it86.cc>
2014-04-20 01:49:37 +0800

    没有可用的Icon，随便添加个Icon，暂时先用着

---
+++ oschina/ZDDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZDRootViewController.h
+//  oschina
+//
+//  Created by royalwang on 14-4-16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDRootViewController : UITableViewController
+
+@end
---
+++ oschina/ZDDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  ZDRootViewController.m
+//  oschina
+//
+//  Created by royalwang on 14-4-16.
+//
+//
+
+#import "ZDRootViewController.h"
+
+@implementation ZDRootViewController
+
+@end
